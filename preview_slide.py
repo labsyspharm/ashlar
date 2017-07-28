8fbe1dd5de8a737675f9cf41420325400b2bb2af
Jeremy Muhlich <jmuhlich@bitflood.org>
2017-07-28 11:02:42 -0400

    Fix missed refactoring changes in preview script

--- preview_slide.py
+++ preview_slide.py
@@ -12,22 +12,22 @@ reader = reg.Reader(filepath)
 metadata = reader.metadata
 
 positions = metadata.positions - metadata.origin
-mshape = ((metadata.positions + metadata.sizes - metadata.origin).max(axis=0) + 1).astype(int)
+mshape = ((metadata.positions + metadata.size - metadata.origin).max(axis=0) + 1).astype(int)
 mosaic = np.zeros(mshape, dtype=np.uint16)
 
 total = reader.metadata.num_images
 for i in range(total):
     sys.stdout.write("\rLoading %d/%d" % (i + 1, total))
     sys.stdout.flush()
-    reg.paste(mosaic, np.flipud(reader.read(c=0, series=i)), positions[i])
+    reg.paste(mosaic, reader.read(c=0, series=i), positions[i])
 print
 
 ax = plt.gca()
 
 modest_image.imshow(ax, mosaic)
 
-widths, heights = np.fliplr(metadata.sizes).T
-for xy, w, h in zip(np.fliplr(positions), widths, heights):
+h, w = metadata.size
+for xy in np.fliplr(positions):
     ax.add_patch(mpatches.Rectangle(xy, w, h, color='black', fill=False))
 
 plt.show()
