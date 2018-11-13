6f790a5c550d9a55c7c6ae496433fb74131208ab
Yu-An Chen <atwood12@gmail.com>
2018-11-13 17:24:45 -0500

    Add subpixel registration and cleanup thumbnail functions

--- ashlar/thumbnail.py
+++ ashlar/thumbnail.py
@@ -1,19 +1,17 @@
 from __future__ import print_function, division
 import sys
 import numpy as np
-import matplotlib.pyplot as plt
-# import matplotlib.patches as mpatches
 from . import reg
 from skimage.transform import rescale
 from skimage.feature import register_translation
 
 
-def make_thumbnail(reader, channel=0, scale=0.05, plot=False, save_thumbnail=False):
+def make_thumbnail(reader, channel=0, scale=0.05):
     
     metadata = reader.metadata
-
     positions = metadata.positions - metadata.origin
-    mshape = (((positions + metadata.size).max(axis=0) + 1) * scale).astype(int)
+    coordinate_max = (positions + metadata.size).max(axis=0) 
+    mshape = ((coordinate_max+ 1) * scale).astype(int)
     mosaic = np.zeros(mshape, dtype=np.uint16)
 
     total = reader.metadata.num_images
@@ -27,12 +25,12 @@ def make_thumbnail(reader, channel=0, scale=0.05, plot=False, save_thumbnail=Fal
 
     return mosaic
 
-def calculate_image_offset(img1, img2):
+def calculate_image_offset(img1, img2, upsample_factor=1):
 
     ref = reg.fft2(reg.whiten(img1))
     test = reg.fft2(reg.whiten(img2))
 
-    shift, error, _ = register_translation(ref, test, 1, 'fourier')
+    shift, error, _ = register_translation(ref, test, upsample_factor, 'fourier')
 
     return shift
 
@@ -41,12 +39,13 @@ def calculate_cycle_offset(reader1, reader2, channel=0, scale=0.05):
     img1 = make_thumbnail(reader1, channel=channel, scale=scale)
     img2 = make_thumbnail(reader2, channel=channel, scale=scale)
 
-    img_offset = calculate_image_offset(img1, img2) * 1/scale
-
+    img_offset = calculate_image_offset(img1, img2, int(1/scale)) * 1/scale
     ori_diff = reader2.metadata.origin - reader1.metadata.origin
+
+    print(
+        'Estimated offset [ y, x] =',
+        img_offset - ori_diff
+    )
 
     return img_offset - ori_diff
 
-
-if __name__ == '__main__':
-    main()
