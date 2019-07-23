1fd1d99dabd16558b0bf663fbd05e0b4497fc78d
Yu-An Chen <atwood12@gmail.com>
2019-07-22 21:56:23 -0400

    Create default dfp and ffp with proper dimension

--- ashlar/reg.py
+++ ashlar/reg.py
@@ -851,10 +851,10 @@ class Mosaic(object):
         if dfp_path or ffp_path:
             c = max(self.channels) + 1
             self.dfp = np.atleast_3d(
-                skimage.io.imread(dfp_path) if dfp_path else np.zeros(c)
+                skimage.io.imread(dfp_path) if dfp_path else np.zeros((1, 1, c))
             )
             self.ffp = np.atleast_3d(
-                skimage.io.imread(ffp_path) if ffp_path else np.ones(c)
+                skimage.io.imread(ffp_path) if ffp_path else np.zeros((1, 1, c))
             )
             # FIXME This assumes integer dtypes. Do we need to support floats?
             self.dfp /= np.iinfo(self.dtype).max
