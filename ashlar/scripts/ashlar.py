95f3dadc609b8c8b941f7a396d2c652192df65b2
Jeremy Muhlich <jmuhlich@bitflood.org>
2020-01-08 15:08:02 -0500

    Drop Python 2 support (#51)
    *Remove compatibility imports and deprecation warning
    * Update dependency versions (including some py3-only releases)
    * Update Dockerfile to run with Python 3 instead of 2

--- ashlar/scripts/ashlar.py
+++ ashlar/scripts/ashlar.py
@@ -2,10 +2,7 @@ from __future__ import print_function
 import sys
 import re
 import argparse
-try:
-    import pathlib
-except ImportError:
-    import pathlib2 as pathlib
+import pathlib
 from .. import __version__ as VERSION
 from .. import reg
 from ..reg import PlateReader, BioformatsReader
@@ -15,12 +12,6 @@ from ..zen import ZenReader
 
 
 def main(argv=sys.argv):
-
-    if sys.version_info.major < 3:
-        print("===========================================================")
-        print("** NOTICE: Python 2 support will end on January 1, 2020. **")
-        print("===========================================================")
-        print()
 
     parser = argparse.ArgumentParser(
         description='Stitch and align one or more multi-series images'
